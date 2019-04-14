b4759d5a8fca8eb5124f7292312da31aae100e9c
Aryan <arykedare@gmail.com>
2019-04-14 10:57:06 +0530

    mido: Add Blobs for Q

--- mido/mido-vendor.mk
+++ mido/mido-vendor.mk
@@ -618,7 +618,11 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/mido/proprietary/vendor/bin/mlipayd:$(TARGET_COPY_OUT_VENDOR)/bin/mlipayd \
     vendor/xiaomi/mido/proprietary/vendor/etc/init/vendor.xiaomi.hardware.mlipay@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.xiaomi.hardware.mlipay@1.0-service.rc \
     vendor/xiaomi/mido/proprietary/vendor/lib64/vendor.xiaomi.hardware.mlipay@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.xiaomi.hardware.mlipay@1.0.so \
-    vendor/xiaomi/mido/proprietary/vendor/lib64/libmlipay.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmlipay.so
+    vendor/xiaomi/mido/proprietary/vendor/lib64/libmlipay.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmlipay.so \
+    vendor/xiaomi/mido/proprietary/vendor/lib/libbfqio.so:$(TARGET_COPY_OUT_VENDOR)/lib/libbfqio.so \
+    vendor/xiaomi/mido/proprietary/vendor/lib64/libbfqio.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbfqio.so \
+    vendor/xiaomi/mido/proprietary/vendor/lib/libmedia.so:$(TARGET_COPY_OUT_VENDOR)/liblibmedia.so \
+    vendor/xiaomi/mido/proprietary/vendor/lib/libmediaextractor.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmediaextractor.so
 
 PRODUCT_PACKAGES += \
     libts_detected_face_hal \
